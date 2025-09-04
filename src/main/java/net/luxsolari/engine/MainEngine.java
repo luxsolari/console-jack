afcc4e29c0f9f0bc58b060f5abca43cffe1776ba
Lux Solari <luxsolari@outlook.com>
2025-09-04 01:59:37 -0300

    chore: add IDE folders to gitignore

---
+++ src/main/java/net/luxsolari/engine/MainEngine.java
@@ -0,0 +1,33 @@
+package net.luxsolari.engine;
+
+import net.luxsolari.engine.systems.internal.MasterSubsystem;
+
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class MainEngine {
+    private static final String VERSION = "0.0.1";
+    private static final String NAME = "Solari Engine v" + VERSION;
+    private static final String TAG = MainEngine.class.getSimpleName();
+    private static final Logger LOGGER = Logger.getLogger(TAG);
+
+    public static void bootstrap(String[] args) {
+        LOGGER.info(NAME);
+        // Load logging configuration
+        try (var is = MainEngine.class.getClassLoader().getResourceAsStream("logging.properties")) {
+            if (is != null) {
+                LogManager.getLogManager().readConfiguration(is);
+            } else {
+                LOGGER.warning("Could not find logging configuration file");
+            }
+        } catch (Exception e) {
+            LOGGER.warning("Could not load logging configuration file");
+        }
+
+        LOGGER.info("[%s] Starting Main".formatted(TAG));
+        // Start Master Subsystem
+        MasterSubsystem.INSTANCE.run();
+
+        LOGGER.info("[%s] Exiting Main".formatted(TAG));
+    }
+}
